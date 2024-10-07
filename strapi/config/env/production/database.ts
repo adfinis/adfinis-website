f73bcfa343a263aed793160904eb51275958f879
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-10-07 17:39:04 +0200

    feat: configure production postgres db

---
+++ strapi/config/env/production/database.ts
@@ -0,0 +1,36 @@
+import path from 'path'
+
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite')
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          ca: env('DATABASE_SSL_CA', undefined),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
+        },
+      },
+      debug: false,
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+  }
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  }
+}
