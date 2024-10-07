9e9fe6d9f049213a912f574dd982cdb3cba97d82
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-10-07 22:03:12 +0200

    chore: redo app config

--- strapi/config/env/production/database.ts
+++ strapi/config/env/production/database.ts
@@ -1,32 +1,16 @@
-import path from 'path'
-
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres')
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'),
-        ssl: {
-          ca: env('DATABASE_SSL_CA', undefined),
-        },
-      },
-      debug: false,
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  }
-
-  return {
+export default ({env}) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
-  }
-}
+    debug: false,
+  },
+})
