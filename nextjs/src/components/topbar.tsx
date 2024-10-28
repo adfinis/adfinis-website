a11208facfa41cb5794ac1309d2fd8bc628cc107
Nathan Alder <nathan.alder@gmail.com>
2024-10-28 20:55:56 +0100

    chore: Add bg shadow for topbar for better contrast

--- nextjs/src/components/topbar.tsx
+++ nextjs/src/components/topbar.tsx
@@ -6,7 +6,10 @@ import NavDesktop from "./nav-desktop"
 const Topbar: React.FC = () => {
   return (
     <div>
-      <div className="fixed top-8 left-0 w-topbar bg-sapphire z-50" id="navbar">
+      <div
+        className="fixed top-8 left-0 w-topbar bg-sapphire z-50 shadow-2"
+        id="navbar"
+      >
         <div className="container mr-0">
           <NavMobile />
           <NavDesktop />
