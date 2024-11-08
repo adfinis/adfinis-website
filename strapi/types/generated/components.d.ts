9a838fb550036abdab8db395b660c2c5874a6252
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-11-09 00:46:34 +0100

    feat (strapi): hero component stub

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface GlobalHero extends Struct.ComponentSchema {
+  collectionName: 'components_global_heroes';
+  info: {
+    displayName: 'Hero';
+    icon: 'picture';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text;
+    Title: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'global.hero': GlobalHero;
+    }
+  }
+}
