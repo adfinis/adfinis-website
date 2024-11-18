66d960d67f29b8f55df0ff40df4401670722b1ad
Nathan Alder <nathan.alder@gmail.com>
2024-11-18 15:54:31 +0100

    chore: Whitelist adfinis.com

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "adfinis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 }
