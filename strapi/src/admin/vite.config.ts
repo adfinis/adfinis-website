474add3a3d829a0c0990de167eab40188a8f90ac
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-11-19 10:20:18 +0100

    fix (strapi): vite config double slash in path

--- strapi/src/admin/vite.config.ts
+++ strapi/src/admin/vite.config.ts
@@ -1,8 +1,8 @@
 import { mergeConfig, type UserConfig } from 'vite';
 
 export default (config: UserConfig) => {
-  const base = config.mode === 'production' ? `/strapi/${config.base}` : config.base;
   console.log('MAU', {base})
+  const base = config.mode === 'production' ? `/strapi${config.base}` : config.base;
   // Important: always return the modified config
   return mergeConfig(config, {
     base,
