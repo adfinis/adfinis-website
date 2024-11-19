ee52f1a198fc54aaa7f5c6b2cf58dbdaccf5b78e
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-11-19 10:20:29 +0100

    chore (strapi build): debug

--- strapi/src/admin/vite.config.ts
+++ strapi/src/admin/vite.config.ts
@@ -1,8 +1,8 @@
 import { mergeConfig, type UserConfig } from 'vite';
 
 export default (config: UserConfig) => {
-  console.log('MAU', {base})
   const base = config.mode === 'production' ? `/strapi${config.base}` : config.base;
+  console.log('MAU', config)
   // Important: always return the modified config
   return mergeConfig(config, {
     base,
