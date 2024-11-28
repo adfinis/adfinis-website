bbbab1323a4ecf859c856e45ea39036d1313fa48
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-11-28 22:43:36 +0100

    feat (strapi): link with chevron component
    button component with external link

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,3 +1,33 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ExternalLinksLinkWithChevron extends Struct.ComponentSchema {
+  collectionName: 'components_external_links_link_with_chevrons';
+  info: {
+    displayName: 'Link with chevron';
+    icon: 'link';
+  };
+  attributes: {
+    chavron: Schema.Attribute.Enumeration<['none', 'left', 'right']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'left'>;
+    href: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'https://adfinis.com/contact'>;
+    is_disabled: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Button text'>;
+    size: Schema.Attribute.Enumeration<['large', 'small']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'large'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'external-links.link-with-chevron': ExternalLinksLinkWithChevron;
+    }
+  }
+}
