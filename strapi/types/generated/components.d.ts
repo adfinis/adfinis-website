d3007f0133f391b12716b144d967326bf4bbb65c
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-11-28 22:48:39 +0100

    feat (strapi): call to action component

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,5 +1,29 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ExternalLinksCallToAction extends Struct.ComponentSchema {
+  collectionName: 'components_external_links_call_to_actions';
+  info: {
+    displayName: 'Call to action';
+    icon: 'link';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'https://adfinis.com/contact'>;
+    is_disabled: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Call to action'>;
+    size: Schema.Attribute.Enumeration<['large', 'small']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'large'>;
+    variant: Schema.Attribute.Enumeration<['cta', 'primary', 'secondary']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'cta'>;
+  };
+}
+
 export interface ExternalLinksLinkWithChevron extends Struct.ComponentSchema {
   collectionName: 'components_external_links_link_with_chevrons';
   info: {
@@ -27,6 +51,7 @@ export interface ExternalLinksLinkWithChevron extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'external-links.call-to-action': ExternalLinksCallToAction;
       'external-links.link-with-chevron': ExternalLinksLinkWithChevron;
     }
   }
