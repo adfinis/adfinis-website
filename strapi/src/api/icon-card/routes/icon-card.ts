a65e0473a4b177a9ee920caf01fa46617f4a9662
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-12-06 11:46:58 +0100

    feat (strapi): add icon card collection

---
+++ strapi/src/api/icon-card/routes/icon-card.ts
@@ -0,0 +1,7 @@
+/**
+ * icon-card router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::icon-card.icon-card');
---
+++ strapi/src/api/icon-card/services/icon-card.ts
@@ -0,0 +1,7 @@
+/**
+ * icon-card service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::icon-card.icon-card');
