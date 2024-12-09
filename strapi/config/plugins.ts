75025684eddae1b0efda7b14587259aadf7211fa
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2024-12-09 22:16:06 +0100

    fix: npm install and plugin warning

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -3,15 +3,17 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        rootPath: env('DO_SPACE_DIRECTORY'),
-        credentials: {
-          accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-          secretAccessKey: env('DO_SPACE_SECRET_KEY'),
-        },
-        region: env('DO_SPACE_REGION'),
-        endpoint: env('DO_SPACE_ENDPOINT'),
-        params: {
-          Bucket: env('DO_SPACE_BUCKET'),
+        s3Options: {
+          rootPath: env('DO_SPACE_DIRECTORY'),
+          credentials: {
+            accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+            secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+          },
+          region: env('DO_SPACE_REGION'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          params: {
+            Bucket: env('DO_SPACE_BUCKET'),
+          },
         },
       },
       actionOptions: {
