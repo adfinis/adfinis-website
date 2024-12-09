4a1b082ea9cede23307def7e473e7a0e45150cde
Nathan Alder <nathan.alder@gmail.com>
2024-12-09 20:47:59 +0100

    chore: Separate content

--- nextjs/src/app/[locale]/theme/raw/page.tsx
+++ nextjs/src/app/[locale]/theme/raw/page.tsx
@@ -2,9 +2,33 @@
 import Text from "@/components/text"
 import Title from "@/components/title"
 import Button from "@/components/button"
-import Link from "@/components/link"
-import { example } from "../texts"
+import Link from "@/components/link-button"
 
+const example = `
+# Heading 1 **Example**
+## Heading 2 **Example**
+### Heading 3 **Example**
+
+Originating in **Switzerland**, Adfinis draws *inspiration* from the ***majestic Swiss Alps***, embodying the spirit of exploration and the pursuit of excellence. We provide cutting-edge technology services, including ~~manual work~~ consulting, software development, and managed services, to help organizations optimize their IT environments and boost operational efficiency.  Much like a journey through mountainous terrain, our deep expertise in various technologies and our strong commitment to open-source principles guide you through the complexities of IT landscapes. With Adfinis, you embark on a path towards greater flexibility, scalability, and security in your IT operations, ensuring your organization reaches new heights of success. Feel free to [visit our website](https://www.adfinis.com/en-US/theme).
+
+Originating in **Switzerland**, Adfinis draws *inspiration* from the ***majestic Swiss Alps***, embodying the spirit of exploration and the pursuit of excellence. We provide cutting-edge technology services, including ~~manual work~~ consulting, software development, and managed services, to help organizations optimize their IT environments and boost operational efficiency.  Much like a journey through mountainous terrain, our deep expertise in various technologies and our strong commitment to open-source principles guide you through the complexities of IT landscapes. With Adfinis, you embark on a path towards greater flexibility, scalability, and security in your IT operations, ensuring your organization reaches new heights of success. Feel free to [visit our website](https://www.adfinis.com/en-US/theme).
+> Blockquote example
+1. Ordered list item 1
+2. Ordered list item 2
+   1. Nested ordered list item 1
+   2. Nested ordered list item 2
+
+Small text.
+
+- Unordered list item 1
+- Unordered list item 2
+   - Nested unordered list item 1
+   - Nested unordered list item 2
+
+### Lists with **long blocks**
+- This is a very long sentence that is meant to test how well the markdown renderer handles text that spans multiple lines without any line breaks, ensuring that the text wraps correctly and maintains readability across different devices and screen sizes.
+- Another extremely long sentence follows, designed to push the limits of the markdown formatting and see if it can gracefully manage extensive content without breaking the layout or causing any unexpected issues in the display, making sure that everything remains user-friendly and visually appealing.
+`
 export default function Buttons() {
   const handleClick = () => {
     console.log("Button clicked!")
