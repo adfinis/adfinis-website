1b8bd8d1a8f92de1d32656770874cd8d9e5fed42
Nathan Alder <nathan.alder@gmail.com>
2024-12-09 20:46:19 +0100

    chore: Add dummy content

--- nextjs/src/app/[locale]/theme/texts.ts
+++ nextjs/src/app/[locale]/theme/texts.ts
@@ -424,31 +424,71 @@ export const slaSection = {
   ],
 } as const
 
-export const example = `
-# Heading 1 **Example**
-## Heading 2 **Example**
-### Heading 3 **Example**
-
-Originating in **Switzerland**, Adfinis draws *inspiration* from the ***majestic Swiss Alps***, embodying the spirit of exploration and the pursuit of excellence. We provide cutting-edge technology services, including ~~manual work~~ consulting, software development, and managed services, to help organizations optimize their IT environments and boost operational efficiency.  Much like a journey through mountainous terrain, our deep expertise in various technologies and our strong commitment to open-source principles guide you through the complexities of IT landscapes. With Adfinis, you embark on a path towards greater flexibility, scalability, and security in your IT operations, ensuring your organization reaches new heights of success. Feel free to [visit our website](https://www.adfinis.com/en-US/theme).
-
-Originating in **Switzerland**, Adfinis draws *inspiration* from the ***majestic Swiss Alps***, embodying the spirit of exploration and the pursuit of excellence. We provide cutting-edge technology services, including ~~manual work~~ consulting, software development, and managed services, to help organizations optimize their IT environments and boost operational efficiency.  Much like a journey through mountainous terrain, our deep expertise in various technologies and our strong commitment to open-source principles guide you through the complexities of IT landscapes. With Adfinis, you embark on a path towards greater flexibility, scalability, and security in your IT operations, ensuring your organization reaches new heights of success. Feel free to [visit our website](https://www.adfinis.com/en-US/theme).
-> Blockquote example
-1. Ordered list item 1
-2. Ordered list item 2
-   1. Nested ordered list item 1
-   2. Nested ordered list item 2
-
-Small text.
-
-- Unordered list item 1
-- Unordered list item 2
-   - Nested unordered list item 1
-   - Nested unordered list item 2
-
-### Lists with **long blocks**
-- This is a very long sentence that is meant to test how well the markdown renderer handles text that spans multiple lines without any line breaks, ensuring that the text wraps correctly and maintains readability across different devices and screen sizes.
-- Another extremely long sentence follows, designed to push the limits of the markdown formatting and see if it can gracefully manage extensive content without breaking the layout or causing any unexpected issues in the display, making sure that everything remains user-friendly and visually appealing.
-`
+export const partnerSection = {
+  title: "## Security Technological Ecosystem",
+  partners: [
+    {
+      title: "HashiCorp Vault",
+      description:
+        "HashiCorp Vault is secure software for managing secrets, authentication tokens, and encryption keys. It provides robust access control and audit capabilities.",
+      imageUrl: "/svg/logo/hashicorp-vault-logo.svg",
+      ctas: [
+        {
+          text: "Get started",
+          href: "/contact",
+          variant: "primary",
+          size: "small",
+        },
+        {
+          text: "More info",
+          href: "/services",
+          variant: "secondary",
+          size: "small",
+        },
+      ],
+    },
+    {
+      title: "HashiCorp Consul",
+      description:
+        "HashiCorp Consul is a service networking solution to connect and secure services across any runtime platform and public or private cloud.",
+      imageUrl: "/svg/logo/consul-logo.svg",
+      ctas: [
+        {
+          text: "Get started",
+          href: "/contact",
+          variant: "primary",
+          size: "small",
+        },
+        {
+          text: "More info",
+          href: "/services",
+          variant: "secondary",
+          size: "small",
+        },
+      ],
+    },
+    {
+      title: "StackRox",
+      description:
+        "StackRox is a Kubernetes-native security platform. It protects cloud-native applications across the full life cycle by providing visibility, vulnerability management, compliance, and runtime threat detection.",
+      imageUrl: "/svg/logo/stackrox-logo.svg",
+      ctas: [
+        {
+          text: "Get started",
+          href: "/contact",
+          variant: "primary",
+          size: "small",
+        },
+        {
+          text: "More info",
+          href: "/services",
+          variant: "secondary",
+          size: "small",
+        },
+      ],
+    },
+  ],
+} as const
 
 export const footer = {
   columns: [
