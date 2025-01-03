67f3228bac7019962d351573e8c0303ff73373ee
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-01-03 13:20:31 +0100

    feat (strapi)!: find page by slug
    instead of documentId, breaking default strapi behaviour

--- strapi/src/api/page-partner-and-product/controllers/page-partner-and-product.ts
+++ strapi/src/api/page-partner-and-product/controllers/page-partner-and-product.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::page-partner-and-product.page-partner-and-product');
+export default factories.createCoreController('api::page-partner-and-product.page-partner-and-product', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+
+    const entity = await strapi.documents('api::page-partner-and-product.page-partner-and-product').findFirst({
+      filters: { slug },
+      ...query,
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
