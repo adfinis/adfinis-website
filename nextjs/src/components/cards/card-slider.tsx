8a3f3303e697fadc45a35329fb2f83f7d88ced2b
Nathan Alder <nathan.alder@gmail.com>
2025-01-15 22:41:14 +0100

    fix: Remove ml-auto which can lead to page overflow
    Also adjust for card shadows with additional padding classes

--- nextjs/src/components/cards/card-slider.tsx
+++ nextjs/src/components/cards/card-slider.tsx
@@ -6,6 +6,7 @@ import Link from "../link-button"
 import CardSliderExplanation from "./card-slider-explanation"
 import type { CTA } from "@/lib/cta"
 import ButtonGroup from "../button-group"
+import clsx from "clsx"
 
 type CardSliderProps = {
   title: string
@@ -20,13 +21,18 @@ const CardSlider: React.FC<CardSliderProps> = ({
   children,
 }) => {
   return (
-    <div className="w-topbar ml-auto">
+    <div className="w-topbar">
       <div className="flex flex-col items-start gap-8 flex-1 self-stretch pb-8 sm:hidden">
         <h3 className="text-30 font-semibold text-biscay">{title}</h3>
         <Text markdown={description} />
       </div>
 
-      <div className="flex overflow-x-auto gap-x-6 snap-x snap-mandatory lg:snap-none pb-4 overscroll-x-none pr-6 rtl:pl-6">
+      <div
+        className={clsx([
+          "flex overflow-x-auto gap-x-6 snap-x snap-mandatory lg:snap-none  overscroll-x-none",
+          "pt-2 pb-4 px-4 -ml-4 sm:ml-0 sm:px-2",
+        ])}
+      >
         <CardSliderExplanation title={title} description={description}>
           <ButtonGroup ctas={ctas} />
         </CardSliderExplanation>
