09b4afd1380df3abfb28a0dbd74bd38127d28551
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-01-27 22:50:49 +0100

    feat (AW-28): add events overview route to nextjs

--- nextjs/src/app/[locale]/events/page.tsx
+++ nextjs/src/app/[locale]/events/page.tsx
@@ -6,10 +6,12 @@ import Intro from "@/components/intro"
 import Text from "@/components/text"
 import { renderSections } from "@/components/dynamic-zone/render-sections"
 import Footer from "@/components/stapi/footer"
-import { SLUGS } from "@/app/[locale]/(partners-products)/slugs"
+import CardGroup from "@/components/cards/card-group"
+import Container from "@/components/container"
+import CardArticle from "@/components/cards/card-article"
 
 export default async function EventsOverviewPage({ params: { locale } }) {
-  const url = `events-overview/?locale=${locale}&populate=hero.color&populate=hero.background_image`
+  const url = `events-overview/?locale=${locale}`
   const data = await strapi(url)
   const currentLocale = {
     href: `/${locale}/events`,
@@ -31,6 +33,10 @@ export default async function EventsOverviewPage({ params: { locale } }) {
 
   const { hero, intro, sections } = data
 
+  const cards = await strapi(
+    `event-pages/?locale=${locale}&populate=card_image&populate=hero.background_image`,
+  )
+
   return (
     <>
       <NavProvider>
@@ -41,6 +47,26 @@ export default async function EventsOverviewPage({ params: { locale } }) {
         <Intro>
           <Text markdown={intro} className="grid gap-8" />
         </Intro>
+      )}
+      {cards.length > 0 && (
+        <Container
+          id="event-grid-example"
+          padding="both-padding"
+          background="neutral"
+        >
+          <CardGroup maxWidth="none">
+            {cards.map((event, index) => (
+              <CardArticle
+                key={event.documentId.slice(-4)}
+                title={event.metadata_title}
+                subtitle={event.address}
+                description={event.date_event}
+                imageUrl={event.hero?.background_image.url}
+                logoUrl={event.card_image?.url}
+              />
+            ))}
+          </CardGroup>
+        </Container>
       )}
       {sections && sections.length > 0 && sections.map(renderSections)}
       <Footer locale={locale} />
