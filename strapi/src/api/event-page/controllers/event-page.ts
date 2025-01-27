ac690a2d587da1baecb8f420c8eaf6df0cd97748
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-01-27 21:18:46 +0100

    feat (AW-26): support finding events by slug in strapi

--- strapi/src/api/event-page/controllers/event-page.ts
+++ strapi/src/api/event-page/controllers/event-page.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::event-page.event-page');
+export default factories.createCoreController('api::event-page.event-page', ({strapi}) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+
+    const entity = await strapi.documents('api::event-page.event-page').findFirst({
+      filters: { slug },
+      ...query,
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
