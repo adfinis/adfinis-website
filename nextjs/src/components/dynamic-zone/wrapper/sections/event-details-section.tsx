497d89def770982298a249890be0ff6a691031ae
Nathan Alder <nathan.alder@gmail.com>
2025-02-05 18:49:19 +0100

    fix: Center and limit section width

--- nextjs/src/components/dynamic-zone/wrapper/sections/event-details-section.tsx
+++ nextjs/src/components/dynamic-zone/wrapper/sections/event-details-section.tsx
@@ -15,17 +15,19 @@ type Props = {
 export default function EventDetailsSection({ section }: { section: Props }) {
   return (
     <Container background={section.props.background} padding="both-padding">
-      <InfoLabel text={section.info} className="block mb-4" />
-      <Text markdown={section.description} className="mb-8 max-w-4xl" />
-      {section.cta && (
-        <LinkButton
-          href={section.cta.href}
-          size={section.cta.size}
-          variant={section.cta.variant}
-        >
-          {section.cta.label}
-        </LinkButton>
-      )}
+      <div className="mx-auto max-w-4xl">
+        <InfoLabel text={section.info} className="block mb-4" />
+        <Text markdown={section.description} className="mb-8 max-w-4xl" />
+        {section.cta && (
+          <LinkButton
+            href={section.cta.href}
+            size={section.cta.size}
+            variant={section.cta.variant}
+          >
+            {section.cta.label}
+          </LinkButton>
+        )}
+      </div>
     </Container>
   )
 }
