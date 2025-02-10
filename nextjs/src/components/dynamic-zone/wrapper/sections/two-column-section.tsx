921d6f606bd5c82a2820a5600691af84b628e12f
Nathan Alder <nathan.alder@gmail.com>
2025-02-10 10:07:40 +0100

    AW-64 feat: Add optional CTA to TwoColumnSection (#117)
    feat: Add optional CTA to  TwoColumnSection

--- nextjs/src/components/dynamic-zone/wrapper/sections/two-column-section.tsx
+++ nextjs/src/components/dynamic-zone/wrapper/sections/two-column-section.tsx
@@ -2,11 +2,14 @@ import SectionGroup from "@/components/sections/section-group"
 import Text from "@/components/text"
 import Container from "@/components/container"
 import { SectionProps } from "@/components/dynamic-zone/wrapper/section-props"
+import LinkButton from "@/components/link-button"
+import { CTA } from "@/lib/cta"
 
 type Props = {
   props: SectionProps
   left_column: string
   right_column: string
+  cta?: CTA
 }
 
 export default function TwoColumnSection({ section }: { section: Props }) {
@@ -18,6 +21,11 @@ export default function TwoColumnSection({ section }: { section: Props }) {
       <SectionGroup columns={2}>
         <Text markdown={section.left_column} />
         <Text markdown={section.right_column} />
+        {section.cta && (
+          <LinkButton {...section.cta} className="place-self-start">
+            {section.cta.label}
+          </LinkButton>
+        )}
       </SectionGroup>
     </Container>
   )
