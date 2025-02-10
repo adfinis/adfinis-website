5234fa2cbf85314ab595b927b686d4806d0bddff
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-02-11 00:11:19 +0100

    refactor (AW-62): reuse class names

--- nextjs/src/components/form-fields/textarea.tsx
+++ nextjs/src/components/form-fields/textarea.tsx
@@ -1,6 +1,9 @@
 import FormLabel from "@/components/form/form-label"
-import React, { useId } from "react"
-import { cva } from "class-variance-authority"
+import { useId } from "react"
+import {
+  errorMessageClasses,
+  inputClasses,
+} from "@/components/form-fields/class-names"
 
 type Props = {
   name: string
@@ -9,28 +12,6 @@ type Props = {
   errorMessage: string[]
 }
 
-const fieldClasses = cva(
-  [
-    "bg-input-primary/5",
-    "text-input-primary text-16 leading-normal font-normal w-full rounded-md px-4 py-2",
-    "border",
-    "ring-0 active:!ring-0 focus:!ring-0", // resets blue border of chrome when active. This is replaced with the active: modifiers below
-    "focus:border-input-primary active:border-input-primary",
-  ],
-  {
-    variants: {
-      variant: {
-        default: "border-input-primary/20",
-        error: "border-error",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  },
-)
-const errorMessageClasses =
-  "absolute top-0 right-0 text-error text-14 text-right"
 export default function Textarea({
   name,
   label,
@@ -38,12 +19,11 @@ export default function Textarea({
   rows = 5,
 }: Props) {
   const id = useId()
-
   return (
     <div className="grid gap-2 relative">
       <FormLabel id={id} label={label} />
       <textarea
-        className={fieldClasses({
+        className={inputClasses({
           variant: errorMessage.length > 0 ? "error" : "default",
         })}
         id={id}
