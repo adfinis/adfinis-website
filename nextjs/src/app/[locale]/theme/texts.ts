a9b7ac2a44d5d7ab6f8315f8e5b915083ccc6d3d
Nathan Alder <nathan.alder@gmail.com>
2025-02-20 17:33:29 +0100

    Feature/aw 58 team photo component with two (#133)
    * refactor: Add columns for all tailwind screens
    
    * feat: Add CardMember component
    
    * chore: Extend aspect scheme
    
    * chore: Add example
    
    * chore: Increase animation duration

--- nextjs/src/app/[locale]/theme/texts.ts
+++ nextjs/src/app/[locale]/theme/texts.ts
@@ -971,6 +971,89 @@ export const imageSlider = {
   ],
 } as const
 
+export const teamMembers = [
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "John Doe",
+    description: "Data Engineer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl2:
+      "https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+]
+
 export const footer = {
   columns: [
     {
