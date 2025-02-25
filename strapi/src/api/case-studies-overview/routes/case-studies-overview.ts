daafe559110002f310c81103e84d209892206782
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-02-25 22:44:48 +0100

    feat AW-34 (strapi): add case studies overview middleware

--- strapi/src/api/case-studies-overview/routes/case-studies-overview.ts
+++ strapi/src/api/case-studies-overview/routes/case-studies-overview.ts
@@ -4,4 +4,12 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::case-studies-overview.case-studies-overview');
+export default factories.createCoreRouter('api::case-studies-overview.case-studies-overview', {
+  config: {
+    find: {
+      middlewares: [
+        'global::populate-sections-with-intro-and-localizations',
+      ],
+    },
+  },
+});
