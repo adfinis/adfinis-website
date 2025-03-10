2dbf164d306b3758118345e196e65724a93d423b
Mauricio <mauricio.kruijer@gmail.com>
2025-03-10 09:19:12 +0100

    Feature/aw 57 landing pages (#141)
    * feat (strapi page): AW-57 add page resource
    
    * feat (strapi): AW-57 add dynamic zone components
    
    * feat (strapi): AW-57 support middleware and slug query on api
    
    * feat (nextjs): AW-57 render "pages" resource

---
+++ strapi/src/api/page/controllers/page.ts
@@ -0,0 +1,22 @@
+/**
+ * page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::page.page', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+
+    const entity = await strapi.documents('api::page.page').findFirst({
+      filters: { slug },
+      ...query,
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
+
