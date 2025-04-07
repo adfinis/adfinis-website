db9c5f327c0f35e2d43e9fa02e875b43d5a39732
Nathan <nathan.alder@gmail.com>
2025-04-07 21:19:15 +0200

    chore: Rename

--- nextjs/src/components/cards/card-location.tsx
+++ nextjs/src/components/cards/card-location.tsx
@@ -6,7 +6,7 @@ import Title from "../title"
 const CardLocation: React.FC<Card> = ({ description, title }) => {
   return (
     <div
-      data-component="CardIcon"
+      data-component="CardLocation"
       className="bg-white rounded-xl p-6 shadow-2 flex flex-col min-h-56"
     >
       <Title level={3} boldness={"semibold"} className="mb-6">
