4356de00b53a6f4cf93e433a4c60d7be730737f9
Nathan <nathan.alder@gmail.com>
2025-05-19 17:05:33 +0200

    chore: Add cache control header to DO plugin

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,18 +1,19 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
         s3Options: {
-          rootPath: env('DO_SPACE_DIRECTORY'),
+          rootPath: env("DO_SPACE_DIRECTORY"),
           credentials: {
-            accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-            secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+            accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+            secretAccessKey: env("DO_SPACE_SECRET_KEY"),
           },
-          region: env('DO_SPACE_REGION'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
+          region: env("DO_SPACE_REGION"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
           params: {
-            Bucket: env('DO_SPACE_BUCKET'),
+            Bucket: env("DO_SPACE_BUCKET"),
+            CacheControl: "public, max-age=31536000", // 1 year: 60*60*24*365 = 31536000
           },
         },
       },
