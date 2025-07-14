1c526e2906b5e7fb5bd23fc7696040f3dfde3a52
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-07-14 15:18:10 +0200

    feat (AW-245): add dynamic metadata to case study pages

--- nextjs/src/app/[locale]/(case-studies)/casestudies/[slug]/page.tsx
+++ nextjs/src/app/[locale]/(case-studies)/casestudies/[slug]/page.tsx
@@ -1,6 +1,24 @@
 import { CASE_STUDIES_SLUGS } from "@/lib/slugs"
 import CaseStudyDetailPage from "@/app/[locale]/(case-studies)/case-study-detail-page"
 import { Locale } from "@/lib/locale"
+import { Metadata } from "next"
+import strapi from "@/lib/strapi"
+
+export async function generateMetadata({
+  params: { locale, slug },
+}: {
+  params: {
+    locale: Locale
+    slug: string
+  }
+}): Promise<Metadata> {
+  const url = `page-case-studies/${slug}?locale=${locale}&status=published`
+  const data = await strapi(url)
+  return {
+    title: data.metadata_title,
+    description: data.metadata_title,
+  }
+}
 
 export default async function DetailPage({
   params: { locale, slug },
