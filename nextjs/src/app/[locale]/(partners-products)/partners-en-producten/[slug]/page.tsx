595e9fed16f36a79d5bc5239f1f622e72cd055bf
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-07-14 15:47:19 +0200

    feat (AW-245): add dynamic metadata to partners products pages

--- nextjs/src/app/[locale]/(partners-products)/partners-en-producten/[slug]/page.tsx
+++ nextjs/src/app/[locale]/(partners-products)/partners-en-producten/[slug]/page.tsx
@@ -1,6 +1,25 @@
 import { PARTNER_PRODUCTS_SLUGS } from "@/lib/slugs"
 import PartnersProducts from "@/app/[locale]/(partners-products)/partners-products"
 import { Locale } from "@/lib/locale"
+import { Metadata } from "next"
+import strapi from "@/lib/strapi"
+
+export async function generateMetadata({
+  params: { locale, slug },
+}: {
+  params: {
+    locale: Locale
+    slug: string
+  }
+}): Promise<Metadata> {
+  const url = `page-partner-and-products/${slug}?locale=${locale}&status=published`
+  const data = await strapi(url)
+
+  return {
+    title: data.metadata_title,
+    description: data.metadata_description,
+  }
+}
 
 export default async function PartnerAndProductsPage({
   params: { locale, slug },
