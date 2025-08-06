49c88907038e7f21636178b45940b623baeefbf2
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-08-06 09:47:38 +0200

    fix (AW-250): make sure requested format is converted to strapi compatible locale (with upercase)

--- nextjs/src/lib/get-dictionary.server.ts
+++ nextjs/src/lib/get-dictionary.server.ts
@@ -2,9 +2,10 @@ import { type Locale } from "@/lib/locale"
 
 const dictionaries = {
   en: () => import("@/dictionaries/en.json").then((m) => m.default),
+  "en-au": () => import("@/dictionaries/en-au.json").then((m) => m.default),
   nl: () => import("@/dictionaries/nl.json").then((m) => m.default),
   de: () => import("@/dictionaries/de.json").then((m) => m.default),
-  "de-CH": () => import("@/dictionaries/de-CH.json").then((m) => m.default),
+  "de-ch": () => import("@/dictionaries/de-CH.json").then((m) => m.default),
 }
 
 export const getDictionary = async (locale: Locale) => {
