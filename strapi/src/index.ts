3f09afd8a44bd103339dd59d1eca890090e7c511
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-08-12 00:01:29 +0200

    refactor (AW-258): migration script

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -21,298 +21,50 @@ export default {
    */
   bootstrap({ strapi }: { strapi: Core.Strapi }) {
     const locales = ['en', 'en-AU', 'de', 'de-CH', 'nl']
-    async function setInternalNameIconCard() {
-      const target = 'api::icon-card.icon-card'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
+    const apis = [
+      {target: 'api::icon-card.icon-card', column: 'name'},
+      {target: 'api::quote.quote', column: 'name'},
+      {target: 'api::card-product.card-product', column: 'name'},
+      {target: 'api::content-offer.content-offer', column: 'name'},
+      {target: 'api::hallmark.hallmark', column: 'title'},
+      {target: 'api::hero.hero', column: 'name'},
+      {target: 'api::page.page', column: 'metadata_title'},
+      {target: 'api::page-case-study.page-case-study', column: 'metadata_title'},
+      {target: 'api::event-page.event-page', column: 'metadata_title'},
+      {target: 'api::news-page.news-page', column: 'metadata_title'},
+      {target: 'api::page-partner-and-product.page-partner-and-product', column: 'metadata_title'},
+      {target: 'api::solutions-page.solutions-page', column: 'metadata_title'},
+    ]
+    async function setInternalName() {
+      for (const api of apis) {
+        const {target, column} = api
+        for (const locale of locales) {
+          // @ts-ignore
+          const items = await strapi.documents(target).findMany({
+            locale,
+            filters: {
+              // @ts-ignore
+              internal_name: {
+                $null: true
+              }
             }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.name
-            },
           })
+          for (const item of items) {
+            // @ts-ignore
+            await strapi.documents(target).update({
+              documentId: item.documentId,
+              locale: item.locale,
+              data: {
+                // @ts-ignore
+                internal_name: item[column]
+              },
+            })
+          }
         }
       }
     }
-
-    async function setInternalNameQuote() {
-      const target = 'api::quote.quote'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.name
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNameCardProduct() {
-      const target = 'api::card-product.card-product'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.name
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNameContentOffer() {
-      const target = 'api::content-offer.content-offer'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.name
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNameHallmark() {
-      const target = 'api::hallmark.hallmark'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNameHero() {
-      const target = 'api::hero.hero'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.name
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePage() {
-      const target = 'api::page.page'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePageStudy() {
-      const target = 'api::page-case-study.page-case-study'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePageEvent() {
-      const target = 'api::event-page.event-page'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePageNews() {
-      const target = 'api::news-page.news-page'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePagePartnerProducts() {
-      const target = 'api::page-partner-and-product.page-partner-and-product'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-    async function setInternalNamePageSolutions() {
-      const target = 'api::solutions-page.solutions-page'
-      for (const locale of locales) {
-        const items = await strapi.documents(target).findMany({
-          locale,
-          filters: {
-            internal_name: {
-              $null: true
-            }
-          }
-        })
-        console.log(items)
-        for (const item of items) {
-          await strapi.documents(target).update({
-            documentId: item.documentId,
-            locale: item.locale,
-            data: {
-              internal_name: item.metadata_title
-            },
-          })
-        }
-      }
-    }
-
-    async function run() {
-      await setInternalNameIconCard()
-      await setInternalNameCardProduct()
-      await setInternalNameContentOffer()
-      await setInternalNameHallmark()
-      await setInternalNameHero()
-      await setInternalNamePage()
-      await setInternalNamePageStudy()
-      await setInternalNamePageEvent()
-      await setInternalNamePageNews()
-      await setInternalNamePagePartnerProducts()
-      await setInternalNamePageSolutions()
-      await setInternalNameQuote()
-    }
-    run()
+    setInternalName().then(() => {
+      console.log('done setting internal name')
+    })
   },
 };
