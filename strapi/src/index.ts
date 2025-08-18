3160cc8ee1591b9c7c22e4c96e459b30f9a16757
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-08-18 13:44:49 +0200

    chore (AW-258): set migration functions

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -19,25 +19,26 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap({ strapi }: { strapi: Core.Strapi }) {
+  bootstrap({strapi}: { strapi: Core.Strapi }) {
     const locales = ['en', 'en-AU', 'de', 'de-CH', 'nl']
     const apis = [
-      {target: 'api::icon-card.icon-card', column: 'name'},
-      {target: 'api::quote.quote', column: 'name'},
-      {target: 'api::card-product.card-product', column: 'name'},
-      {target: 'api::content-offer.content-offer', column: 'name'},
-      {target: 'api::hallmark.hallmark', column: 'title'},
-      {target: 'api::hero.hero', column: 'name'},
-      {target: 'api::page.page', column: 'metadata_title'},
-      {target: 'api::page-case-study.page-case-study', column: 'metadata_title'},
-      {target: 'api::event-page.event-page', column: 'metadata_title'},
-      {target: 'api::news-page.news-page', column: 'metadata_title'},
-      {target: 'api::page-partner-and-product.page-partner-and-product', column: 'metadata_title'},
-      {target: 'api::solutions-page.solutions-page', column: 'metadata_title'},
+      {target: 'api::icon-card.icon-card', column: 'name', publish: false},
+      {target: 'api::quote.quote', column: 'name', publish: false},
+      {target: 'api::card-product.card-product', column: 'name', publish: false},
+      {target: 'api::content-offer.content-offer', column: 'name', publish: true},
+      {target: 'api::hallmark.hallmark', column: 'title', publish: false},
+      {target: 'api::hero.hero', column: 'name', publish: false},
+      {target: 'api::page.page', column: 'metadata_title', publish: true},
+      {target: 'api::page-case-study.page-case-study', column: 'metadata_title', publish: true},
+      {target: 'api::event-page.event-page', column: 'metadata_title', publish: true},
+      {target: 'api::news-page.news-page', column: 'metadata_title', publish: true},
+      {target: 'api::page-partner-and-product.page-partner-and-product', column: 'metadata_title', publish: true},
+      {target: 'api::solutions-page.solutions-page', column: 'metadata_title', publish: true},
     ]
+
     async function setInternalName() {
       for (const api of apis) {
-        const {target, column} = api
+        const {target, column, publish} = api
         for (const locale of locales) {
           // @ts-ignore
           const items = await strapi.documents(target).findMany({
@@ -59,12 +60,21 @@ export default {
                 internal_name: item[column]
               },
             })
+            if (publish) {
+              // @ts-ignore
+              await strapi.documents(target).publish({
+                documentId: item.documentId,
+                locale: item.locale,
+              })
+            }
           }
         }
       }
     }
-    setInternalName().then(() => {
-      console.log('done setting internal name')
-    })
+
+    // console.log('Start setting internal name')
+    // setInternalName().then(() => {
+    //   console.log('Finished setting internal name!')
+    // })
   },
 };
