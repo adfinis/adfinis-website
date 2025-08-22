4443d180f3e02a69e32cf2b115ab4f8e5c5b62a2
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-08-22 14:43:29 +0200

    feat (AW-267): copy title to description

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -18,5 +18,38 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap({strapi}: { strapi: Core.Strapi }) {},
+  bootstrap({strapi}: { strapi: Core.Strapi }) {
+    async function setTitleAsDescriptionOnNewsPages() {
+      const target = 'api::news-page.news-page';
+      for (const originLocale of ['en', 'en-AU', 'de-CH', 'de-CH', 'nl']) {
+        const docs = await strapi.documents(target).findMany({
+          locale: originLocale,
+          filters: {
+            $or: [
+              {metadata_description: { $null: true }},
+              {metadata_description: { $eq: '' }},
+            ]
+          },
+        });
+        for (const doc of docs) {
+          const {locale, documentId} = doc;
+          await strapi.documents(target).update({
+            documentId,
+            locale,
+            data: {
+              metadata_description: doc.metadata_title
+            },
+          });
+          await strapi.documents(target).publish({
+            documentId,
+            locale: locale,
+          });
+          console.log({documentId})
+        }
+      }
+    }
+    setTitleAsDescriptionOnNewsPages().then(() => {
+      console.log('bruf')
+    })
+  },
 };
