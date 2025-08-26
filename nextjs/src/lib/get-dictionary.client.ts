e382b1153177babd88d4dac2ff471bf86841acab
Nathan <nathan.alder@gmail.com>
2025-08-26 15:04:04 +0200

    chore: Update locale files to lowercase and remove dde

--- nextjs/src/lib/get-dictionary.client.ts
+++ nextjs/src/lib/get-dictionary.client.ts
@@ -4,14 +4,14 @@ import { type Locale } from "@/lib/locale"
 import en from "@/dictionaries/en.json"
 import enAU from "@/dictionaries/en-au.json"
 import nl from "@/dictionaries/nl.json"
-import de from "@/dictionaries/de.json"
-import deCH from "@/dictionaries/de-CH.json"
+import deCH from "@/dictionaries/de-ch-ch.json"
+import deDE from "@/dictionaries/de-de.json"
 
 const dictionaries = {
   en,
   nl,
-  de,
   "de-CH": deCH,
+  "de-DE": deDE,
   "en-AU": enAU,
 } as const
 
