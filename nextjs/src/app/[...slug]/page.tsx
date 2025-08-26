c0c2d924190ba3220f1e29f6b01592050022f607
Mauricio <mauricio.kruijer@gmail.com>
2025-08-27 01:40:05 +0200

    fix (AW-277): 404 page was captured by locae (#231)

---
+++ nextjs/src/app/[...slug]/page.tsx
@@ -0,0 +1,71 @@
+import { getPage } from "@/lib/strapi"
+import { NavProvider } from "@/components/nav-bar/nav-context"
+import NavBar from "@/components/nav-bar/nav-bar"
+import HeroWrapper from "@/components/stapi/hero-wrapper"
+import Intro from "@/components/intro"
+import Text from "@/components/text"
+import { renderSections } from "@/components/dynamic-zone/render-sections"
+import Footer from "@/components/stapi/footer"
+import { Locale } from "@/lib/locale"
+import { Metadata } from "next"
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: {
+    slug: string[]
+  }
+}): Promise<Metadata> {
+  const data = await getPage("en", slug.join("/"))
+  return {
+    title: data.metadata_title,
+    description: data.metadata_description,
+  }
+}
+
+export default async function LandingPage({
+  params: { slug },
+}: {
+  params: { slug: string[] }
+}) {
+  const locale = "en"
+  const URI_PATH = slug.join("/")
+  const activeLocale = {
+    href: `/${locale}/${URI_PATH}`,
+    locale: locale as Locale,
+    isActive: true,
+  }
+  const data = await getPage(locale, URI_PATH)
+  const locales = data.localizations.map(
+    (item: { locale: Locale; slug: string }) => {
+      return {
+        href: `/${item.locale.toLowerCase()}/${item.slug}`,
+        locale: item.locale,
+        isActive: false,
+      }
+    },
+  )
+  locales.push(activeLocale)
+
+  const { hero, intro, sections } = data
+
+  return (
+    <>
+      <NavProvider>
+        <NavBar items={locales} />
+        {hero && <HeroWrapper hero={hero} />}
+      </NavProvider>
+      {intro && (
+        <Intro>
+          <Text markdown={intro} />
+        </Intro>
+      )}
+      {sections &&
+        sections.length > 0 &&
+        sections.map((section: any, index: number) =>
+          renderSections(section, index, activeLocale.locale),
+        )}
+      <Footer locale={activeLocale.locale} />
+    </>
+  )
+}
