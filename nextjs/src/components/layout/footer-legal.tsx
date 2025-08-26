ce704cfb7fc5f89a65d1bd875e33f15ee70e99c1
Nathan Alder <nathan.alder@gmail.com>
2025-08-26 21:43:29 +0200

    feat: Add browser cache and strapi cache for 1h (#228)
    * feat: Add browser cache and strapi cache for 1h
    
    * fix: Lintfix

--- nextjs/src/components/layout/footer-legal.tsx
+++ nextjs/src/components/layout/footer-legal.tsx
@@ -1,12 +1,6 @@
 import React from "react"
-import Link from "next/link"
-type FooterLegalProps = {
-  privacyPolicy: {
-    href: string
-    text: string
-  }
-}
-const FooterLegal: React.FC<FooterLegalProps> = ({ privacyPolicy }) => (
+
+const FooterLegal: React.FC = () => (
   <span className="text-14">&copy; {new Date().getFullYear()} Adfinis.</span>
 )
 
