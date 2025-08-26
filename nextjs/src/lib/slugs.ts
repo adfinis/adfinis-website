4323e46873049691ca35c62686f46a38e6da01cc
Nathan <nathan.alder@gmail.com>
2025-08-26 15:27:29 +0200

    chore: Remove unused slugs

--- nextjs/src/lib/slugs.ts
+++ nextjs/src/lib/slugs.ts
@@ -4,58 +4,38 @@ export const PARTNER_PRODUCTS_SLUGS: Record<Locale, string> = {
   nl: "partners-en-producten",
   en: "partners-and-products",
   "en-au": "partners-and-products",
-  "en-AU": "partners-and-products",
-  de: "partner-und-produkte",
   "de-ch": "partner-und-produkte",
-  "de-CH": "partner-und-produkte",
   "de-de": "partner-und-produkte",
-  "de-DE": "partner-und-produkte",
 } as const
 
 export const CASE_STUDIES_SLUGS: Record<Locale, string> = {
   nl: "casestudies",
   en: "case-studies",
   "en-au": "case-studies",
-  "en-AU": "case-studies",
-  de: "referenzen",
   "de-ch": "referenzen",
-  "de-CH": "referenzen",
   "de-de": "referenzen",
-  "de-DE": "referenzen",
 } as const
 
 export const SOLUTIONS_SLUGS: Record<Locale, string> = {
   nl: "oplossingen",
   en: "solutions",
   "en-au": "solutions",
-  "en-AU": "solutions",
-  de: "loesungen",
   "de-ch": "loesungen",
-  "de-CH": "loesungen",
   "de-de": "loesungen",
-  "de-DE": "loesungen",
 } as const
 
 export const NEWS_SLUGS: { [key: string]: string } = {
   nl: "nieuws",
   en: "news",
   "en-au": "news",
-  "en-AU": "news",
-  de: "news",
   "de-ch": "news",
-  "de-CH": "news",
   "de-de": "news",
-  "de-DE": "news",
 }
 
 export const BLOG_SLUGS: { [key: string]: string } = {
   nl: "blogs",
   en: "blogs",
   "en-au": "blogs",
-  "en-AU": "blogs",
-  de: "blogs",
   "de-ch": "blogs",
-  "de-CH": "blogs",
   "de-de": "blogs",
-  "de-DE": "blogs",
 }
