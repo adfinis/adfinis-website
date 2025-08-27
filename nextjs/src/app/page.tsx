d42a2cb26a4a8225f99245d32803142d28c09bb2
Nathan <nathan.alder@gmail.com>
2025-08-27 15:27:23 +0200

    refactor: Change cookie reading to client without ssr

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,8 +1,5 @@
 import Homepage from "@/app/homepage"
-import CookieNotice from "@/components/cookie-notice"
 import { Locale } from "@/lib/locale"
-
-export const dynamic = "force-dynamic"
 
 export default async function Page() {
   const activeLocale: { href: string; locale: Locale; isActive: boolean } = {
@@ -13,7 +10,6 @@ export default async function Page() {
   return (
     <>
       <Homepage activeLocale={activeLocale} />
-      <CookieNotice locale="en" />
     </>
   )
 }
