0f0f0015311faf76682e7a5a32a2c63a0085bd57
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-09-02 12:31:53 +0200

    fix (AW-298): add missing metadata for homepage

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,8 +1,17 @@
 import Homepage from "@/app/homepage"
 import { Locale } from "@/lib/locale"
 import CookieNotice from "@/components/cookie-notice"
+import { Metadata } from "next"
+import { getHomepage } from "@/lib/strapi"
 
 export const dynamic = "force-dynamic"
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await getHomepage("en")
+  return {
+    title: data.metadata_title,
+    description: data.metadata_description,
+  }
+}
 
 export default async function Page() {
   const activeLocale: { href: string; locale: Locale; isActive: boolean } = {
