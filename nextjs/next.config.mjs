6714426e613f08df043e7d13e5b9af6342bb46c6
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2025-09-23 22:25:22 +0200

    fix (AW-285): add redirect on prod domain

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -50,15 +50,6 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-        ],
-      },
     ]
   },
   async redirects() {
